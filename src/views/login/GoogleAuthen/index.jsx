d2d2eb02371705cf406ba57dd35e3e7770c03b80
devHoangDong <jetaimend@gmail.com>
2021-10-11 09:30:38 +0700

    up test ggauth

--- src/views/login/GoogleAuthen/index.jsx
+++ src/views/login/GoogleAuthen/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useGoogleLogin } from 'react-google-login';
 import { axios } from 'axios';
-
 const GoogleAuthContext = React.createContext()
 
 export const GoogleAuthProvider = ({ children }) => {
@@ -10,28 +9,41 @@ export const GoogleAuthProvider = ({ children }) => {
   //for product
   //const GOOGLE_CLIENT_ID = '818031211174-5j50gsf9knjqec9j0lcgo05v34irl1ni.apps.googleusercontent.com'
   const getToken = async (authCode) => {
-  const result = await axios.post('https://oauth2.googleapis.com', {
-  client_id:'117400138061-qmmhcm296ol3tqa2tc0eq60a5j553c27.apps.googleusercontent.com',
-  client_secret:'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&code=4/0AX4XfWgHDADYsVvMPWaB2Jwuc-cjF5TzyhuSWy66nv2z84NJ_rQj5WehsPBc-_1PiuyT0g',
-  code:{authCode},
-  grant_type:'authorization_code',
-  redirect_uri:'https://localhost:3000'
-})
-  let data = result;
-    console.log(data);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("client_id", "117400138061-qmmhcm296ol3tqa2tc0eq60a5j553c27.apps.googleusercontent.com");
+    urlencoded.append("client_secret", "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");
+    urlencoded.append("grant_type", "authorization_code");
+    urlencoded.append("redirect_uri", "https://developers.google.com/oauthplayground");
+    urlencoded.append("code", authCode);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    fetch("https://oauth2.googleapis.com/token", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
+  const test = 'https://accounts.google.com/o/oauth2/auth?client_id=117400138061-qmmhcm296ol3tqa2tc0eq60a5j553c27.apps.googleusercontent.com&response_type=token&redirect_uri=https://developers.google.com/oauthplayground&scope=https://www.googleapis.com/auth/drive'
   const googleAuth = useGoogleLogin({
     clientId: GOOGLE_CLIENT_ID, // Your clientID from Google.
     isSignedIn: true,
-    scope:'https://www.googleapis.com/auth/drive',
-    responseType:'code',
-    accessType:'offline',
-    onSuccess:response => console.log(response.code),
+    scope: 'https://www.googleapis.com/auth/drive',
+    responseType: 'code',
+    accessType: 'offline',
+    onSuccess: response => getToken(response.code),
     // render={renderProps => (
     //   <button onClick={renderProps.onClick} >This is my custom Google button</button>
     // )}
-    buttonText:"Login",
-    cookiePolicy:'single_host_origin',
+    buttonText: "Login",
+    cookiePolicy: 'single_host_origin',
   })
 
   return (
